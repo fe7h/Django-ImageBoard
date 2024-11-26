02376c5c7b9b61cd6cacbfe7822b5556cfb3c8a8
fesh <189043093+fe7h@users.noreply.github.com>
2024-11-26 03:40:14 +0200

    in threads added view and model message created

--- threads/models.py
+++ threads/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    data = models.TextField()
+    time_create = models.DateTimeField(auto_now_add=True)
--- threads/views.py
+++ threads/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def thread(request):
+    return render(request,'threads/thread.html')

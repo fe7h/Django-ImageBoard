e324132ef8dcf8e8120f3a3901a9870848602a4c
fesh <189043093+fe7h@users.noreply.github.com>
2024-12-07 18:33:40 +0200

    add simple form in html

--- threads/views.py
+++ threads/views.py
@@ -3,5 +3,9 @@ from .models import Message
 
 # Create your views here.
 def thread(request):
+    if request.method == 'POST':
+        if request.POST['test']:
+            print(request.POST['test'])
+            Message.objects.create(data=request.POST['test'])
     data = Message.objects.all()
     return render(request,'threads/thread.html', context={'data' : data})

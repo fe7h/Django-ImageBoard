eca7e18acd035615fec35d2a84705098ee7033cc
fesh <189043093+fe7h@users.noreply.github.com>
2024-12-09 22:36:01 +0200

    added redirect in thread view

--- threads/views.py
+++ threads/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Message
 
 # Create your views here.
 def thread(request):
     if request.method == 'POST':
         if request.POST['test']:
-            print(request.POST['test'])
             Message.objects.create(data=request.POST['test'])
+        return redirect(thread)
     data = Message.objects.all()
     return render(request,'threads/thread.html', context={'data' : data})

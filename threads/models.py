2ff8e0cbd34ac79ddf64a6f5aeb5f52040137507
fesh <189043093+fe7h@users.noreply.github.com>
2024-12-12 18:59:33 +0200

    Images upload added

--- threads/models.py
+++ threads/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Message(models.Model):
     data = models.TextField()
     time_create = models.DateTimeField(auto_now_add=True)
+
+
+class Image(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='images')
+    img = models.ImageField(upload_to='threads/images/')
--- threads/views.py
+++ threads/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Message
+from .models import Message, Image
 
 # Create your views here.
 def thread(request):
     if request.method == 'POST':
         if request.POST['test']:
-            Message.objects.create(data=request.POST['test'])
+            print(request.POST)
+            msg = Message.objects.create(data=request.POST['test'])
+            if request.FILES:
+                print(request.FILES)
+                for file in request.FILES.getlist('img'):
+                    print(type(file))
+                    Image.objects.create(message=msg, img=file)
         return redirect(thread)
     data = Message.objects.all()
     return render(request,'threads/thread.html', context={'data' : data})

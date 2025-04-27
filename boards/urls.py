177d36110c3424a465a93d86c610cce201c6fb0c
fesh <189043093+fe7h@users.noreply.github.com>
2025-04-27 20:24:45 +0300

    changed urls in boards app

--- boards/urls.py
+++ boards/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from . import views
 
 
+board_internal_patterns = [
+    path('<slug:board_slug>/', views.BoardDetailView.as_view(), name='board-show'),
+    path('<slug:board_slug>/thread/', include('threads.urls')),
+]
+
 urlpatterns = [
     # path('', views.board_list, name='board-list'),
-    path('board/<slug:board_slug>/', views.BoardDetailView.as_view(), name='board-show'),
-    path('board/<slug:board_slug>/thread/', include('threads.urls')),
+    path('board/', include((board_internal_patterns, None))),
 ]
